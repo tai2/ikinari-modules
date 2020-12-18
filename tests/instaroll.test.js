2652ff630ed4e7f0174eb5856a3cd5aa2af6df60
tai2 <hello@tai2.net>
2020-12-19 00:02:29 +0900

    Add tests

---
+++ tests/instaroll.test.js
@@ -0,0 +1,42 @@
+const { spawn, execFileSync } = require("child_process")
+
+const binPath = "../bin/instaroll"
+
+function compile(sourceCode) {
+  return execFileSync(binPath, ["-i", "-"], {
+    input: sourceCode,
+  }).toString()
+}
+
+function run(sourceCode) {
+  return execFileSync("node", [], {
+    input: sourceCode,
+  }).toString()
+}
+
+test("import from unpkg.com", () => {
+  const sourceCode = `
+import stringLength from 'https://unpkg.com/string-length'
+console.log(stringLength('🐴'))`
+  const compiledCode = compile(sourceCode)
+  expect(compiledCode).not.toMatch(/^import/)
+  expect(run(compiledCode)).toBe("1\n")
+})
+
+test("import from stkypack.com", () => {
+  const sourceCode = `
+import stringLength from 'https://cdn.skypack.dev/string-length'
+console.log(stringLength('🐴'))`
+  const compiledCode = compile(sourceCode)
+  expect(compiledCode).not.toMatch(/^import/)
+  expect(run(compiledCode)).toBe("1\n")
+})
+
+test("import from node_modules", () => {
+  const sourceCode = `
+import stringLength from 'string-length'
+console.log(stringLength('🐴'))`
+  const compiledCode = compile(sourceCode)
+  expect(compiledCode).not.toMatch(/^import/)
+  expect(run(compiledCode)).toBe("1\n")
+})
